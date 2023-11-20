e9afb38b5a678e42aa28acc2fb1ea37cebd1452d
Andrew Cross <crossy1985@gmail.com>
2023-11-20 13:17:07 +0000

    WIP

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # ETCMC-Node-Win-Firewall-Rules
+
+## Important
+
+In order for the .bat file to amend your windows firewall rules, you must launch the command prompt as an administrator
--- WFModify.bat
+++ WFModify.bat
@@ -1,7 +1,8 @@
 @echo off
 setlocal enabledelayedexpansion
 
-set RULES_FILE=FirewallRules.txt
+set SCRIPT_DIR=%~dp0
+set RULES_FILE=%SCRIPT_DIR%FirewallRules.txt
 
 if not exist %RULES_FILE% (
     echo Error: Rules file not found!
@@ -18,9 +19,11 @@ for /f "tokens=1,2,3" %%a in (%RULES_FILE%) do (
     for %%d in (!DIRECTIONS!) do (
         if /i %%d equ inbound (
             echo Allow Port !PORT! Inbound Protocol: !PROTOCOL!
+            netsh advfirewall firewall add rule name="Allow Port !PORT! Inbound" dir=in action=allow protocol=!PROTOCOL! localport=!PORT!
         ) else (
             if /i %%d equ outbound (
                 echo Allow Port !PORT! Outbound Protocol: !PROTOCOL!
+                netsh advfirewall firewall add rule name="Allow Port !PORT! Outbound" dir=out action=allow protocol=!PROTOCOL! localport=!PORT!
             ) else (
                 echo Invalid direction: %%d
             )
@@ -31,23 +34,7 @@ for /f "tokens=1,2,3" %%a in (%RULES_FILE%) do (
 set /p CONFIRM=Do you want to apply these firewall rules? (Y/N):
 
 if /i "%CONFIRM%" equ "Y" (
-    for /f "tokens=1,2,3" %%a in (%RULES_FILE%) do (
-        set PORT=%%a
-        set PROTOCOL=%%b
-        set DIRECTIONS=%%c
-
-        for %%d in (!DIRECTIONS!) do (
-            if /i %%d equ inbound (
-                netsh advfirewall firewall add rule name="Allow Port !PORT! Inbound" dir=in action=allow protocol=!PROTOCOL! localport=!PORT!
-            ) else (
-                if /i %%d equ outbound (
-                    netsh advfirewall firewall add rule name="Allow Port !PORT! Outbound" dir=out action=allow protocol=!PROTOCOL! localport=!PORT!
-                ) else (
-                    echo Invalid direction: %%d
-                )
-            )
-        )
-    )
+    echo Applying firewall rules...
     echo Firewall rules added successfully.
 ) else (
     echo Operation cancelled by user.
